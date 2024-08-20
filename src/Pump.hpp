fd634c1a71acc79acf645c25dd576db7461ae8ca
mahyar1284 <mahyarshokraeian@live.com>
2024-08-20 20:01:16 +0330

    Pump Merge Bug Fixed

--- src/Pump.hpp
+++ src/Pump.hpp
@@ -1,37 +1,21 @@
 #ifndef PUMP_HPP
 #define PUMP_HPP
-
 #include <SPIFFS.h>
 #include <string.h>
 #include "Configuration.hpp"
-#include "Time.hpp"
-#include <Arduino.h>
-#include "IObserver.hpp"
 #include "SystemTime.hpp"
-#include <string.h>
+#include "IObserver.hpp"
 
-class Pump: public IObserver<SystemTime>
+class Pump
 {
-public:
-    static Pump *getInstance();
-    bool getPumpState();
-    void manualSwitch(signed manualTime = -1);
-    void loop(int currentHours, int currentMinutes);
-    void update(SystemTime *systemTime)
-    {
-        auto schList = Configuration::getInstance()->getSchedulerList(); // get scheduler list from configuration
-        Scheduler sch(schList);
-        digitalWrite(25, sch.isAnyItemOn());
-        Serial.println("pump pin is " + getPumpState() ? "on" : "off");
-    }
-    bool getPumpState()
-    {
-        return digitalRead(25);
-    }
-
 private:
     static Pump *instance;
     Pump();
+
+public:
+    static Pump *getInstance();
+    bool getPumpState();
+    void update(SystemTime *systemTime);
 };
 
-#endif // PUMP_HPP
+#endif
