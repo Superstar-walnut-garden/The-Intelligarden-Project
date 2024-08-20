740e108af2e28f39fad6ec093c81f2f49b1018bd
mahyar1284 <mahyarshokraeian@live.com>
2024-08-20 21:50:09 +0330

    Build Script For WebApp Added

---
+++ WebBuildScript.py
@@ -0,0 +1,13 @@
+Import("env")
+import os
+
+def before_build_spiffs(source, target, env):
+    print("Building React App...")
+    env.Execute("cd intelligarden-web-app && npm run build")
+    print("React App built!")
+    print("Removing old SPIFFS image...")
+    env.Execute("rm -rf data")
+    print("Copying React App to SPIFFS...")
+    env.Execute("cp -r intelligarden-web-app/dist/* data/dist")
+
+env.AddPreAction("$BUILD_DIR/spiffs.bin", before_build_spiffs)
