fdf2e1d6119f24c3a869bc7131ae0d9763be5f61
Majid Moghaddam <fake@example.com>
2024-09-19 10:36:47 +0330

    Modified to work with FirabaseManager

--- src/CppVirtualInterface.cpp
+++ src/CppVirtualInterface.cpp
@@ -2,14 +2,6 @@
 #include <ESP32Time.h>
 #include <FirebaseESP32.h>
 #include <time.h>
-#include "addons/TokenHelper.h"
-#include "addons/RTDBHelper.h"
-#define API_KEY "AIzaSyAsGvcYkkIhaLzNqt6YIt7DVEskcAKUHig"
-#define DATABASE_URL "https://the-intelligarden-project-default-rtdb.firebaseio.com/" 
-#define USER_EMAIL "IntelligardenNodeAznab@walnut.garden"
-// #define USER_EMAIL "demo@walnut.garden"
-#define USER_PASSWORD "ThisIsThePasscode"
-#define DATABASE_ROOT_NAME "AznabNode1/"
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -27,15 +19,10 @@
 #include "Temperature.hpp"
 #include "Display.hpp"
 
+#include "FirebaseManager.hpp"
+
 constexpr double nightTemp = 23.0, dayTemp = 25.0;
 constexpr double threshold = 0.05;
-
-
-
-
-
-
-FirebaseData fbdo;
 
 FirebaseAuth auth;
 FirebaseConfig config;
@@ -45,7 +32,8 @@ String uid;
 bool signupOK = false;
 bool firebaseOK = true;
 
-
+FBData fbd;
+FirebaseManager fbm(fbd);
 
 int virtualMain()
 {
@@ -76,6 +64,8 @@ int virtualMain()
     webInterface->init();
     Serial.println("Sensors and web interface are initialized!");
     
+    //FirebaseManager fbm(fbdo);
+
     if(wifiSetup->isConnected())
     {
         systemTime->obtainTime();
@@ -83,42 +73,8 @@ int virtualMain()
         {
             Serial.println("");
             Serial.println("WiFi connected.");
-            // ------------- firebase setup ----------------
-            // Assign the api key (required)
-            config.api_key = API_KEY;
-            /* Assign the RTDB URL (required) */
-            config.database_url = DATABASE_URL;
-            // Assign the user sign in credentials
-            auth.user.email = USER_EMAIL;
-            auth.user.password = USER_PASSWORD;
-            fbdo.setResponseSize(4096);
-            // Assign the callback function for the long running token generation task
-            config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
 
-            // Assign the maximum retry of token generation
-            config.max_token_generation_retry = 5;
-            // Initialize the library with the Firebase authen and config
-            Firebase.begin(&config, &auth);
-            // Getting the user UID might take a few seconds
-            Serial.println("Getting User UID");
-            while ((auth.token.uid) == "")
-            {
-                Serial.print('.');
-                delay(1000);
-                static int localCounter = 0;
-                if(localCounter ++ > 6)
-                {
-                    localCounter = 0;
-                    Serial.println("Error: Couldn't get user's UID");
-                    firebaseOK = false;
-                    break;
-                }
-            }
-            // Print user UID
-            uid = auth.token.uid.c_str();
-            Serial.print("User UID: ");
-            Serial.println(uid);
-            Firebase.reconnectWiFi(true);
+            fbm.init();
         }
     }
 
@@ -148,45 +104,7 @@ int virtualMain()
         }
         if(systemTime->isTimeUpdated())
         {
-            int hour = systemTime->getHour();
-            int minute = systemTime->getMinute();
-            Serial.printf("Internal RTC Time: %.2d:%.2d\n", hour, minute);
-            systemTime->notifierEngine();
-            if((minute % 10) == 0)
-            {
-                if(firebaseOK)
-                {
-                    auto *cfg = Configuration::getInstance();
-                    auto sensorList = cfg->getSensorList();
-                    for(auto sensor : sensorList)
-                    {
-                        auto name = sensor.getName();
-                        auto value = temperature->getData(name);
-                        auto databasePath = DATABASE_ROOT_NAME + std::to_string(systemTime->getYear()) + "/" + std::to_string(systemTime->getMonth()) + "/" 
-                                    + std::to_string(systemTime->getDay()) + "/" + std::to_string(systemTime->getHour());
-                        temperature->read();
-                        if(Firebase.RTDB.getFloat(&fbdo, databasePath + "/" + name) == NULL)
-                        {
-                            Serial.println("new data is about to be registered on the database!");
-                            if (Firebase.RTDB.setFloat(&fbdo, databasePath + "/" + name, temperature->getData(name)))
-                            {
-                                Serial.println("PASSED");
-                                Serial.println("PATH: " + fbdo.dataPath());
-                                Serial.println("TYPE: " + fbdo.dataType());
-                            }
-                            else
-                            {
-                                Serial.println("FAILED");
-                                Serial.println("REASON: " + fbdo.errorReason());
-                            }
-                        }
-                        else
-                        {
-                            Serial.println("warning: the data for the current time and date is already registered on the database!");
-                        }
-                    }
-                }
-            }
+            fbm.update(systemTime);
         }
         else
             Serial.println("warning: time is not available due to connection error at the system startup!");
