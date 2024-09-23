1c43ff51151479d63964ab4c12a6145319e4b996
Majid Moghaddam <fake@example.com>
2024-09-23 09:33:59 +0330

    Add enabaled and Null Check

--- src/FBData.hpp
+++ src/FBData.hpp
@@ -19,13 +19,18 @@ class FBData
     std::string getUserEmail();
     std::string getUserPassword();
     std::string getDatabaseRootName();
+    bool isEnabled();
+    bool isNull();
 
     private:
+    bool FBDataNull(std::string data);
+
     std::string apiKey;
     std::string databaseURL;
     std::string userEmail;
     std::string userPassword;
     std::string databaseRootName;
+    bool enabled;
 };
 
 
