adaac600251b155ecc78a57350619b7a802d92b2
mahyar1284 <mahyarshokraeian@live.com>
2025-02-14 23:52:32 +0330

    added BaseItem class

---
+++ src/BaseItem.hpp
@@ -0,0 +1,30 @@
+#ifndef BASEITEM_HPP
+#define BASEITEM_HPP
+#include <string>
+#include <ArduinoJson.h>
+
+class BaseItem 
+{
+public:
+    BaseItem();
+    BaseItem(int id, int event_id, std::string name, bool status);
+    virtual ~BaseItem();
+
+    virtual int getId() const;
+    virtual void setId(int id);
+    virtual int getEventId() const;
+    virtual void setEventId(int event_id);
+    virtual std::string getName() const;
+    virtual void setName(std::string name);
+    virtual bool getStatus() const;
+    virtual void setStatus(bool status);
+    virtual void populateFromJson(std::string json);
+    virtual std::string toJson();
+
+private:
+    int id, event_id;
+    std::string name;
+    bool status;
+};
+
+#endif // BASEITEM_HPP
