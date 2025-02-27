9b03d72cedd8e78ebe66192354d139417b19ff0c
mahyar1284 <mahyarshokraeian@live.com>
2025-02-27 16:52:31 +0330

    added char lcd support

--- src/Display.cpp
+++ src/Display.cpp
@@ -3,9 +3,28 @@
 Display* Display::instance = nullptr;
 
 Display::Display()
-    : oled(U8G2_R0, U8X8_PIN_NONE, 22, 21), text("Booting up...")
+    : oled(U8G2_R0, U8X8_PIN_NONE, 22, 21), text("Booting up..."), charLcd(0x3F, 2, 16), displayType("char")
 {
-    oled.begin();
+    if(displayType == "oled")
+    {
+        oled.begin();
+    }
+    else if(displayType == "char")
+    {
+        charLcd.init();
+        charLcd.clear();         
+        charLcd.backlight(); // Make sure backlight is on
+
+        charLcd.setCursor(4, 1);
+        charLcd.print("Loading!");
+
+        charLcd.setCursor(1, 0);
+        charLcd.print("Intelligarden");
+    }
+    else
+    {
+        Serial.println("Error: Invalid display type. supported types are 'oled' and 'char'");
+    }
 }
 
 Display* Display::getInstance()
@@ -17,11 +36,18 @@ Display* Display::getInstance()
 
 void Display::drawMultiLineText(int x, int y, const char* text)
 {
-    int lineHeight = 10; // Adjust line height as needed
+    int lineHeight = 10; // for OLED
+    if(displayType == "char")
+        lineHeight = 1; // for char lcd
     char* line = strtok((char*)text, "\n");
     while (line != NULL)
-    {
-        oled.drawStr(x, y, line);
+    {   if(displayType == "oled")
+            oled.drawStr(x, y, line);
+        else if(displayType == "char")
+        {
+            charLcd.setCursor(0, y);
+            charLcd.print(line);
+        }
         y += lineHeight; // Move to the next line
         line = strtok(NULL, "\n");
     }
@@ -39,8 +65,17 @@ void Display::drawUI()
     oled.clearBuffer();
     oled.drawFrame(0, 0, 125, 63);
     oled.setFont(u8g2_font_ncenB08_tr);    // choose a suitable font
-    drawMultiLineText(2, 10, text.c_str());
-    oled.sendBuffer();
+    if(displayType == "oled")
+    {
+        drawMultiLineText(2, 10, text.c_str());
+        oled.sendBuffer();
+    }
+    else if(displayType == "char")
+    {
+        charLcd.clear();
+        drawMultiLineText(0, 0, text.c_str());
+    }
+    
 }
 
 void Display::update(Temperature *temp)
