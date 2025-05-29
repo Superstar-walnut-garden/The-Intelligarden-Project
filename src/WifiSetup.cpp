b6067b31d94ee7de25d228bef20e29382c463226
mahyar1284 <mahyarshokraeian@live.com>
2025-05-29 23:04:34 +0300

    Enhance WifiSetup class with connection status callbacks and internet connectivity check functionality

--- src/WifiSetup.cpp
+++ src/WifiSetup.cpp
@@ -2,10 +2,17 @@
 #include <thread>
 
 
-// Static member variable initialization
+/**
+ * @brief instance initialization for the singleton pattern
+ * 
+ */
 WifiSetup *WifiSetup::instance = nullptr;
 
-// Singleton instance getter
+/**
+ * @brief retrieve the singleton instance of WifiSetup.
+ * 
+ * @return WifiSetup* 
+ */
 WifiSetup *WifiSetup::getInstance()
 {
     if (!instance)
@@ -13,29 +20,58 @@ WifiSetup *WifiSetup::getInstance()
     return instance;
 }
 
-// Check if connected to WiFi
-bool WifiSetup::isConnected()
-{
-    return WiFi.status() == WL_CONNECTED;
-}
 
-// Constructor
+/**
+ * @brief Constructor for WifiSetup class.
+ * Initializes the Wi-Fi connection and starts an internal async thread to monitor the connection status.
+ */
 WifiSetup::WifiSetup()
 {
     auto wifiCred = Configuration::getInstance()->getWifiCredentials();
     // Start the Wi-Fi reconnect thread
     std::thread wifiThread([this, wifiCred]()
     {
+        bool lastOnlineStatus = false; // Track last online status
+        bool lastConnectedStatus = false; // Track last wifi connection status
         while(true)
         {
             if(!isConnected()) // if not connected
                 connect(wifiCred); // retry
-            std::this_thread::sleep_for(std::chrono::minutes(2)); // Check every 2 minutes
+            if(isConnected() && !lastConnectedStatus) // if connected to wifi
+            {
+                Serial.println("Wifi Thread: Connected to WiFi!");
+                connectFlag.store(true); // Invoke onConnect callback
+                lastConnectedStatus = true; // Update last connected status
+            }
+            else if(!isConnected() && lastConnectedStatus) // if disconnected from wifi
+            {
+                Serial.println("Wifi Thread: Disconnected from WiFi!");
+                disconnectFlag.store(true); // Invoke onDisconnect callback
+                lastConnectedStatus = false; // Update last connected status
+            }
+            if(isOnline() && !lastOnlineStatus) // if connected to wifi and online
+            {
+                Serial.println("Wifi Thread: Internet is available now!");
+                onlineFlag.store(true); // Invoke onOnline callback
+                lastOnlineStatus = true; // Update last online status
+            }
+            else if(!isOnline() && lastOnlineStatus) // if disconnected from internet
+            {
+                Serial.println("Wifi Thread: Internet is disconnected!");
+                offlineFlag.store(true); // Invoke onOffline callback
+                lastOnlineStatus = false; // Update last online status
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(15)); // Check every 15 seconds
         }
     });
     wifiThread.detach(); // Run independently
 }
 
+/**
+ * @brief Connect to Wi-Fi using the provided credentials.
+ * 
+ * @param wifiCred The Wi-Fi credentials to use for connection.
+ */
 void WifiSetup::connect(WifiHotspotData wifiCred)
 {
     auto ssid = wifiCred.getSsid();
@@ -80,18 +116,88 @@ void WifiSetup::connect(WifiHotspotData wifiCred)
         WiFi.disconnect();
         WiFi.mode(WIFI_AP); // turn off wifi and only work with hotspot
     }
-    if(isConnected())
-        connectFlag.store(true); // Raise flag
 }
 
+/**
+ * @brief Set the callback function to be called when connected to Wi-Fi.
+ * 
+ * @param callback The function to call when connected.
+ */
 void WifiSetup::onConnect(std::function<void()> callback)
 {
     onConnectCallback = callback;
 }
 
+/**
+ * @brief Set the callback function to be called when online (internet available).
+ * 
+ * @param callback The function to call when online.
+ */
+void WifiSetup::onOnline(std::function<void()> callback)
+{
+    onOnlineCallback = callback;
+}
+
+/**
+ * @brief Set the callback function to be called when disconnected from Wi-Fi.
+ * 
+ * @param callback The function to call when disconnected.
+ */
+void WifiSetup::onDisconnect(std::function<void()> callback)
+{
+    onDisconnectCallback = callback;
+}
+
+/**
+ * @brief Set the callback function to be called when offline (internet not available).
+ * 
+ * @param callback The function to call when offline.
+ */
+void WifiSetup::onOffline(std::function<void()> callback)
+{
+    onOfflineCallback = callback;
+}
+
+/**
+ * @brief Check if the device is online (connected to the internet).
+ * 
+ * @return true if online, false otherwise.
+ */
+bool WifiSetup::isOnline()
+{
+    if(isConnected()) // If connected to wifi, check internet connectivity
+        if (Ping.ping(IPAddress(8, 8, 8, 8), 3)) // Ping Google's dns server
+            return true;
+    return false;
+}
+
+/**
+ * @brief Check if the device is connected to Wi-Fi (regardless of internet availability).
+ * 
+ * @return true if connected, false otherwise.
+ */
+bool WifiSetup::isConnected()
+{
+    return WiFi.status() == WL_CONNECTED;
+}
+
+/**
+ * @brief Main loop function to manage connection callbacks and flags.
+ * This function should be called periodically to handle Wi-Fi events.
+ */
 void WifiSetup::loop()
 {
-    if (connectFlag.exchange(false)) // Check and reset flag atomically
+    // manage the connection callbacks and flags
+    if (connectFlag.exchange(false)) // Check and reset wifi connect flag atomically
         if (onConnectCallback) 
             onConnectCallback();
+    if(onlineFlag.exchange(false)) // Check and reset online flag atomically
+        if (onOnlineCallback) 
+            onOnlineCallback();
+    if (disconnectFlag.exchange(false)) // Check and reset disconnect flag atomically
+        if (onDisconnectCallback) 
+            onDisconnectCallback();
+    if (offlineFlag.exchange(false)) // Check and reset offline flag atomically
+        if (onOfflineCallback) 
+            onOfflineCallback();
 }
