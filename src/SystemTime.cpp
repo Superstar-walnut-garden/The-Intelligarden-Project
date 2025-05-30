979e077e6e3596c1d355d822580f7cbc37310099
mahyar1284 <mahyarshokraeian@live.com>
2025-05-30 14:36:17 +0330

    Add TimeConfigData integration to SystemTime class

--- src/SystemTime.cpp
+++ src/SystemTime.cpp
@@ -1,5 +1,5 @@
 #include "SystemTime.hpp"
-
+#include "Configuration.hpp"
 
 /**
  * @brief instance initialization for the singleton pattern
@@ -24,16 +24,22 @@ SystemTime *SystemTime::getInstance()
  * 
  */
 SystemTime::SystemTime() 
-: rtc(0), timeClient(ntpUDP), timeUpdated(false), externalRTCEnabled(false), setTimeAutomatically(true), ntpUpdated(false)
+: rtc(0)
 {
+    loadState(); // load the saved configuraion state from SPIFFS
+    // set realtime variables startup default values
+    currentConfigData.setExternalRTCAvailability(false);
+    currentConfigData.setTimeSubsystemInitialized(false);
+    currentConfigData.setNtpUpdated(false);
+
     if(externalRTC.begin()) // try to initialize module
     {
         Serial.println("External RTC module found!");
         Serial.println("External RTC sub-system initialized successfully!");
-        externalRTCEnabled = true;
+        currentConfigData.setExternalRTCAvailability(true);
         DateTime now = externalRTC.now();
         rtc.setTime(now.second(), now.minute(), now.hour(), now.day(), now.month(), now.year());
-        timeUpdated = true;
+        currentConfigData.setTimeSubsystemInitialized(true);
     }
 }
 
@@ -43,7 +49,8 @@ SystemTime::SystemTime()
  */
 void SystemTime::obtainTime()
 {
-    if(setTimeAutomatically and !ntpUpdated)
+    auto& cfg = currentConfigData;
+    if(cfg.isSetTimeAutomatically() and !cfg.isNtpUpdated())
     {
         const char* ntpServer = "pool.ntp.org";
         const long  gmtOffset_sec = 12600;
@@ -74,9 +81,9 @@ void SystemTime::obtainTime()
                 Serial.println("Time obtained successfully!");
                 Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
                 Serial.println("-----------------------");
-                timeUpdated = true;
-                ntpUpdated = true;
-                if(externalRTCEnabled)
+                cfg.setTimeSubsystemInitialized(true);
+                cfg.setNtpUpdated(true);
+                if(cfg.isExternalRTCAvailable())
                 {
                     externalRTC.adjust(DateTime(timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec));
                     Serial.println("External RTC sub-system updated successfully!");
@@ -84,7 +91,7 @@ void SystemTime::obtainTime()
             }
             else
             {
-                if(externalRTCEnabled) // resync with external RTC if available
+                if(cfg.isExternalRTCAvailable()) // resync with external RTC if available
                 {
                     DateTime now = externalRTC.now();
                     rtc.setTime(now.second(), now.minute(), now.hour(), now.day(), now.month(), now.year());
@@ -109,7 +116,7 @@ void SystemTime::obtainTime()
  */
 bool SystemTime::isTimeUpdated()
 {
-    return timeUpdated;
+    return currentConfigData.isTimeSubsystemInitialized();
 }
 
 /**
@@ -174,12 +181,67 @@ void SystemTime::lostTrackOfTime()
     Serial.printf("reset reason core 1 code: %d\n", rtc_get_reset_reason(1));
     if (!(rtc_get_reset_reason(0) == 0xc || rtc_get_reset_reason(1) == 0xc)) // if reset cause is not from the watchdog
     {
-        if(!externalRTCEnabled) // if there is no external rtc module
+        if(!currentConfigData.isExternalRTCAvailable()) // if there is no external rtc module
         {
             Serial.println("reset cause is power failure and the internal rtc is reset");
-            timeUpdated = false;
+            currentConfigData.setTimeSubsystemInitialized(false);
             Serial.println("Warning: Lost track of time. Time may be inaccurate!");
             //rtc.setTime(0, 0, 5, 1, 1, 2024); // estimate time.
         }
     }
+}
+
+/**
+ * @brief Get the current configuration of the system time (for web-api purposes).
+ * 
+ * @return std::string 
+ */
+std::string SystemTime::getConfig()
+{
+    return currentConfigData.toJson();
+}
+
+/**
+ * @brief Set the configuration of the system time (for web-api purposes).
+ * 
+ * @param configJson 
+ */
+void SystemTime::setConfig(const std::string& configJson)
+{
+    auto newConfig = TimeConfigData(configJson);
+    // keeping realtime variables intact to prevent wrong
+    newConfig.setTimeSubsystemInitialized(currentConfigData.isTimeSubsystemInitialized());
+    newConfig.setExternalRTCAvailability(currentConfigData.isExternalRTCAvailable());
+    newConfig.setNtpUpdated(currentConfigData.isNtpUpdated());
+    currentConfigData = newConfig; // update the time configuration data
+}
+
+/**
+ * @brief Save the current state of the system time to internal storage.
+ * This function is called to save the current time and configuration to persistent storage.
+ */
+void SystemTime::saveState()
+{
+    Configuration::getInstance()->setTimeConfig(currentConfigData.toJson());   
+}
+
+/**
+ * @brief load the saved state of the system time from internal storage.
+ * This function is called to load the previously saved time and configuration from persistent storage.
+ */
+void SystemTime::loadState()
+{
+    auto state = Configuration::getInstance()->getTimeConfig();
+    if (state.empty())
+        return;
+    currentConfigData.populateFromJson(state);
+}
+
+/**
+ * @brief set the system time using an epoch timestamp.
+ * only works if setTimeAutomatically is false and the manualTimeSetFlag is raised
+ */
+void SystemTime::setTime(unsigned long epochTime)
+{
+    
 }
