c80fad211156ea66f56911d094fc66a4cd07ae34
mahyar1284 <mahyarshokraeian@live.com>
2025-07-10 20:26:11 +0330

    Feat: Implemented SignalNameResolver::parse() method + some modifications to other methods as well

--- src/SignalNameResolver.hpp
+++ src/SignalNameResolver.hpp
@@ -21,11 +21,26 @@ public:
     
     static std::string toString(SignalNameParameters p)
     {
-        return p.subsystemName + std::to_string(p.id) + p.localSignalName + "_" + signalTypeToString(p.type);
+        return p.subsystemName + "_" + std::to_string(p.id) + "_" + p.localSignalName; // localSignalName already includes type (e.g. "_B")
     }
     static SignalNameParameters parse(std::string signalPath)
     {
-        // not implemented yet
+        std::string subsystemName, id, localSignalName, type;
+        auto firstDelim = signalPath.find('_');
+        auto secondDelim = signalPath.find('_', firstDelim + 1);
+        auto thirdDelim = signalPath.find('_', secondDelim + 1);
+
+        subsystemName = signalPath.substr(0, firstDelim);
+        id = signalPath.substr(firstDelim + 1, secondDelim - firstDelim -1);
+        localSignalName = signalPath.substr(secondDelim + 1, thirdDelim - secondDelim -1);
+        type = signalPath.substr(thirdDelim + 1);
+
+        SignalNameParameters snp;
+        snp.subsystemName = subsystemName;
+        snp.id = std::stoi(id);
+        snp.localSignalName = localSignalName;
+        snp.type = parseType(type);
+        return snp;
     }
     static SignalType parseType(std::string signalPath)
     {
@@ -33,16 +48,23 @@ public:
             return SignalType::Broadcaster;
         return SignalType::Listener;
     }
+    // underscore, hyphen or space ain't allowed in the name
     static std::string generateLocalSignalName(std::string locallyUniqueName, SignalType type)
     {
-        return locallyUniqueName + signalTypeToString(type);
+        return locallyUniqueName + "_" + signalTypeToString(type);
     }
 private:
     static std::string signalTypeToString(SignalType type)
     {
         if(type == SignalType::Broadcaster)
-            return "_B";
-        return "_L";
+            return "B";
+        return "L";
+    }
+    static SignalType parseSignalType(std::string type)
+    {
+        if(type == "B")
+            return SignalType::Broadcaster;
+        return SignalType::Listener;
     }
 };
 
