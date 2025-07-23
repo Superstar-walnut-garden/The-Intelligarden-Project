5b0512ba9096f2f06f81ab49a472c4a36ea2bc73
mahyar1284 <mahyarshokraeian@live.com>
2025-07-23 20:38:54 +0330

    Cleanup Event Subsystem residues from WebApiManager

--- src/WebApiManager.cpp
+++ src/WebApiManager.cpp
@@ -1,8 +1,14 @@
 #include "WebApiManager.hpp"
+#include <SPIFFS.h>
+#include <ArduinoJson.h>
+
 #include "Configuration.hpp"
-#include "EventManager.hpp"
 #include "CentralizedSignalHub.hpp"
+#include "SystemTime.hpp"
+#include "GPIOManager.hpp"
+#include "ThermostatManager.hpp"
 #include "SignalManager.hpp"
+#include "Scheduler.hpp"
 
 /**
  * @brief Construct a new WebApiManager object
@@ -125,7 +131,6 @@ void WebApiManager::init()
     createIManagerEndpoints<TempSensorItem>("/TempSensor", Temperature::getInstance());
     createIManagerEndpoints<GPIOItem>("/GPIO", GPIOManager::getInstance());
     createIManagerEndpoints<ThermostatItem>("/Thermostat", ThermostatManager::getInstance());
-    createIManagerEndpoints<EventItem>("/Event", EventManager::getInstance());
     createIManagerEndpoints<SchedulerItem>("/Scheduler", Scheduler::getInstance());
     createIManagerEndpoints<SignalItem>("/signal", SignalManager::getInstance());
     server.begin();
