3adb2a0a33dd82c98047b1a8ed9d418f22f985b1
mahyar1284 <mahyarshokraeian@live.com>
2025-08-16 18:42:22 +0330

    fix signalrouter name typo

--- src/SignalRouterService.hpp
+++ src/SignalRouterService.hpp
@@ -42,7 +42,7 @@ private:
     SignalRouterService(const SignalRouterService&) = delete;
     SignalRouterService& operator=(const SignalRouterService&) = delete;
 
-    std::string getName() override { return "Signal"; };
+    std::string getName() override { return "SignalRouter"; };
     std::vector<ISignalCompatibleItem *> getSignalCompatibleItems() override;
 
     static SignalRouterService* instance;
