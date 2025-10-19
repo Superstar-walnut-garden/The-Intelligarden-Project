9e940dd52585d78ebccd56243d01fbe86d16ea5b
mahyar1284 <mahyarshokraeian@live.com>
2025-10-19 04:58:56 +0330

    Refactor ILoggableItem to keep track of the lastLogTime

--- src/ILoggableItem.hpp
+++ src/ILoggableItem.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <optional>
+#include <chrono>
+#include <ctime>
 
 class ILoggableItem
 {
@@ -11,8 +13,27 @@ public:
     virtual uint64_t getId() const = 0;
     virtual uint64_t getInterval() const = 0;
     virtual bool logOnlyOnChange() const = 0;
-    virtual std::optional<std::string> getData() const = 0;
+    virtual std::string getData() const = 0;
     virtual bool isLoggingEnabled() const = 0;
 
+    virtual bool shouldLog(std::chrono::time_point<std::chrono::system_clock> now) const
+    {
+        return std::chrono::duration_cast<std::chrono::seconds>(now - lastLogTime).count() >= getInterval();
+    }
+
+    // updates lastLogTime and returns data
+    virtual void setLastLogTime(std::chrono::time_point<std::chrono::system_clock> now)
+    {
+        lastLogTime = now;
+    }
+
+    virtual std::chrono::time_point<std::chrono::system_clock> getLastLogTime() const
+    {
+        return lastLogTime;
+    }
+
     virtual ~ILoggableItem() = default; // this line is super important. Never forget it!
+
+private:
+    std::chrono::time_point<std::chrono::system_clock> lastLogTime;
 };
