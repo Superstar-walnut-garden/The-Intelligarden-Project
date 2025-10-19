cb41e91ffcf7a9701741297f59eebdd31ea28941
mahyar1284 <mahyarshokraeian@live.com>
2025-10-19 05:02:25 +0330

    refactored ILoggableService to have a method to set the actual item's lastLogTime

--- src/ILoggableService.hpp
+++ src/ILoggableService.hpp
@@ -12,4 +12,5 @@ public:
 
     virtual std::string getName() const = 0;
     virtual std::vector<std::unique_ptr<ILoggableItem>> getLoggableItems() const = 0;
+    virtual void setLastLogTime(uint64_t itemId, std::chrono::time_point<std::chrono::system_clock> time) = 0;
 };
