8c75799e022a6de0b61955a7d35c36f29969106b
mahyar1284 <mahyarshokraeian@live.com>
2025-12-10 12:39:08 +0330

    remove volatile last log time tracking to do real last log time extraction from external storage data registry

--- src/ILoggableItem.hpp
+++ src/ILoggableItem.hpp
@@ -16,24 +16,5 @@ public:
     virtual std::string getData() const = 0;
     virtual bool isLoggingEnabled() const = 0;
 
-    virtual bool shouldLog(std::chrono::time_point<std::chrono::system_clock> now) const
-    {
-        return std::chrono::duration_cast<std::chrono::seconds>(now - lastLogTime).count() >= getInterval();
-    }
-
-    // updates lastLogTime and returns data
-    virtual void setLastLogTime(std::chrono::time_point<std::chrono::system_clock> now)
-    {
-        lastLogTime = now;
-    }
-
-    virtual std::chrono::time_point<std::chrono::system_clock> getLastLogTime() const
-    {
-        return lastLogTime;
-    }
-
     virtual ~ILoggableItem() = default; // this line is super important. Never forget it!
-
-private:
-    std::chrono::time_point<std::chrono::system_clock> lastLogTime;
 };
