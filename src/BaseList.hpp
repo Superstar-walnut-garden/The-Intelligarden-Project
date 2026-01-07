8f5b8f1a834d896b2eb3e56b42276637ca968e7b
mahyar1284 <mahyarshokraeian@live.com>
2026-01-07 14:51:33 +0330

    Refactor BaseList class to store unique_ptr of objects instead of raw copy of them

--- src/BaseList.hpp
+++ src/BaseList.hpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <type_traits>
+#include <memory> // added for unique_ptr
 #include "BaseItem.hpp"
 
 template <class T>
@@ -14,23 +15,27 @@ class BaseList
     static_assert(std::is_base_of<BaseItem, T>::value, "T must derive from BaseItem");
 public:
     BaseList();
+    ~BaseList() {};
     BaseList(std::string json);
-    virtual T &getItem(uint64_t id);
-    virtual std::vector<T> getList() const;
-    virtual std::string toJson() const;
-    virtual void addItem(T Item);
+    virtual T *getItem(uint64_t id);
+    virtual std::vector<std::unique_ptr<T>> &getList(); // changed return type
+    virtual std::string toJson(std::function<bool(const T*)> filter = nullptr) const;
+    virtual void addItem(std::unique_ptr<T> Item); // changed parameter type
     virtual void repopulateWith(std::string json);
     virtual void printList();
-    virtual void modifyItem(uint64_t id, T& newItem);
+    virtual void modifyItem(uint64_t id, std::unique_ptr<T> newItem); // changed parameter type
     virtual void deleteItem(uint64_t id);
 
-    virtual void forEach(std::function<void(T&)> func);
+    virtual void forEach(std::function<void(T*)> func, std::function<bool(const T*)> filter = nullptr) const;
 
-protected:
-    virtual std::vector<T> &getListRef();
+    template <class I>
+    I* getAs(uint64_t id) const ;
+
+    template <class I, class F = T>
+    std::vector<I*> getAllAs(std::function<bool(const F*)> filter = nullptr) const;
 
 private:
-    std::vector<T> list;
+    std::vector<std::unique_ptr<T>> list; // changed to unique_ptr
 };
 
 #include "BaseList.tpp"
